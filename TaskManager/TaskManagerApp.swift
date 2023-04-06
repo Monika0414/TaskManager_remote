795442ccf348d9b56ec326194d3ecca8192c6e86
Smonique <121874599+Smonique@users.noreply.github.com>
2023-04-06 16:14:42 +0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Monika on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
